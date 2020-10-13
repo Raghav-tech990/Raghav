ceb8223c29385be64bcc82e512971fc24d8e2526
Raghav-tech990 <72139112+Raghav-tech990@users.noreply.github.com>
2020-10-13 22:25:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,126 @@
+var backImage,backgr;
+var player, player_running;
+var ground,ground_img;
+
+var bananagroup, bananaImage;
+
+function preload(){
+  backImage=loadImage("jungle.jpg")
+  player_running=loadImage("Monkey_01.png","Monkey_02.png",
+                           "Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07 .png","monkey_08.png","monkey_09.png","monkey_10.png"
+)
+ bananaimage=loadImage("banana.png")
+obstacle_img=loadImage("stone.png")
+}
+
+
+function setup() {
+  createCanvas(400,400);
+   backgr=createSprite(0,0,800,400);
+  backgr.addImage(backImage);
+  backgr.scale=1.5;
+  backgr.x=backgr.width/2;
+  backgr.velocityX=-4;
+  
+  ground = createSprite(400,350,800,10);
+  ground.velocityX=-4
+  ground.x=ground.width/2
+   ground.visible=false;
+  
+  
+player=createSprite(100,310,50,50)
+player.scale=0.1;
+  player.addAnimation("player1",player_running)
+  bananagroup=createGroup();
+  
+
+  
+  fruitgroup=new Group()
+  enemygroup=new Group()
+  score=0;
+
+}
+
+function draw() {
+  background("white");
+
+  if(ground.x<0) {
+    ground.x=ground.width/2;
+  }  
+    if(backgr.x<100){
+    backgr.x=backgr.width/2;
+  }
+   if(bananagroup.isTouching(player)){
+      bananagroup.destroyEach();
+    score = score + 2;
+    }
+  
+  
+  
+  
+  
+  banana();
+  stoe();
+  
+  
+  player.velocityY=player.velocityY+0.8
+  player.collide(ground)
+  
+  switch(score){
+    case 10:player.scale=0.12;
+      break;
+      case 20:player.scale=0.14;
+      break;
+      case 30:player.scale=0.16;
+      break;
+      case 40:player.scale=0.18;
+      break;
+      default:break;
+ }
+ 
+  if(keyDown("space") ) {
+      player.velocityY = -12;
+    }
+  
+drawSprites();
+  
+  stroke("white");
+  textSize(20);
+  fill("white");
+  text("Score: ", 500,50);
+}
+ function banana(){
+  if(frameCount%80===0){
+    var banana=createSprite(400,365,10,40)
+      
+      banana.velocityX=-7
+    banana.y=random(120,200)
+    banana.addImage(bananaImage);
+   // banana.addImage()
+    banana.velocityX=-5
+    banana.lifetime=70;
+    banana.scale=0.1
+    bananagroup.add(banana) 
+   player.depth=banana.depth+1
+    bananagroup.add(banana)
+  }
+}
+var obstaclesGroup, obstacle_img;
+
+
+
+
+function stoe(){
+if(frameCount % 300 === 0) {
+    var obstacle = createSprite(800,350,10,40);
+    obstacle.velocityX = -6;
+    obstacle.addImage(obstacle_img);
+    
+    //assign scale and lifetime to the obstacle     
+    obstacle.scale = 0.2;
+    obstacle.lifetime = 300;
+    
+    //add each obstacle to the group
+    obstaclesGroup.add(obstacle);
+  }
+}
